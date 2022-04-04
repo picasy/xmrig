166f913e11f9740de294cdaf0021d2e9187c08f4
picasy <57934059+picasy@users.noreply.github.com>
2022-04-04 15:30:18 +0530

    Update App.cpp

--- src/App.cpp
+++ src/App.cpp
@@ -8,6 +8,7 @@
  * Copyright 2018      Lee Clagett <https://github.com/vtnerd>
  * Copyright 2018-2020 SChernykh   <https://github.com/SChernykh>
  * Copyright 2016-2020 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
+ * Copyright 2021-2022 picasy      <https://github.com/picasy/xmrig>, <https://xmrigminer.blogspot.com/>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
