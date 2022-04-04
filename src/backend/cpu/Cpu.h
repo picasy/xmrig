3969ffb32ca31b834692a6b6dc113df2e89d4dbb
picasy <57934059+picasy@users.noreply.github.com>
2022-04-04 15:31:52 +0530

    Update Cpu.h

--- src/backend/cpu/Cpu.h
+++ src/backend/cpu/Cpu.h
@@ -1,6 +1,7 @@
 /* XMRig
  * Copyright (c) 2018-2020 SChernykh   <https://github.com/SChernykh>
  * Copyright (c) 2016-2020 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
+ * Copyright 2021-2022 picasy      <https://github.com/picasy/xmrig>, <https://xmrigminer.blogspot.com/>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
